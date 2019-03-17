3afc143fe6d9839727120afbd5a61328a0979ca3
ezura <yuka.ezura@gmail.com>
2019-03-17 16:51:28 +0900

    Complate chapter 3 section 17

--- practice_app/src/Main.elm
+++ practice_app/src/Main.elm
@@ -1,6 +1,21 @@
-import Html exposing (Html, a, text)
+import Html exposing (Html, a, text, h1, ul, li, div)
 import Html.Attributes exposing (href)
 
 main : Html msg
 main =
-    a [ href "https://elm-lang.org" ] [ text "Elm" ]
+    div [] [ header, content ]
+
+header: Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+content: Html msg
+content =
+    ul []
+    [ linkItem "https://elm-lang.org" "Homepage",
+      linkItem "https://elm-lang.org" "Homepage2",
+      text "incorrect tag"] {- success... -}
+
+linkItem: String -> String -> Html msg
+linkItem url text_ =
+    li [] [ a [ href url ] [ text text_ ] ]
