4b4ebb0e7665e03c223ced4a4ae67c7443b31770
ezura <yuka.ezura@gmail.com>
2019-03-17 20:38:10 +0900

    Create template

--- chapter3_ex1/src/Main.elm
+++ chapter3_ex1/src/Main.elm
@@ -1,1 +1,50 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+init : Model
+init =
+    {}
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Msg
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    text ""
